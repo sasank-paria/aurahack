0dc482adc2d480590110fa450a0148c7d3f50a72
Vaibhav203 <bhav.bhaliya@gmail.com>
2023-12-13 20:31:20 +0530

    rahul

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Reac,{ useState} from 'react'
+
+const Navbar = () => {
+    const[isMenuOpen,setIsMenuOpen] = useState(false);
+    const handleMenuToggler = ()=>{
+        setIsMenuOpen(!isMenuOpen)
+    }
+      return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar

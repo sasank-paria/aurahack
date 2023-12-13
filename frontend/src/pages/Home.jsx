feaed73419b61ccf33443773434abfa96d853467
Gaurav <gauravyadav7403@student.sfit.ac.in>
2023-12-14 02:12:58 +0530

    banner gaurav

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,8 @@
-// import React from 'react'
-import Banner  from '../components/Banner'
+import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-    hass
-      <Banner/>
-    </div>
+    <div>Home</div>
   )
 }
 

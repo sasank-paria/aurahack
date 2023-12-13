bd9abfe0609141016ff37b17fe0dd679bd34e80d
rahulll18 <109649389+rahulll18@users.noreply.github.com>
2023-12-14 04:08:20 +0530

    Rahul Salary.jsx Created

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,11 +4,36 @@ import Banner  from '../components/Banner'
 import Sidebar from '../sideBar/Sidebar'
 
 const Home = () => {
+  const[selectedCategory,setSelectedCategory]= useState(null);
+  const[jobs, setJobs]=useState();
   const [isLoading , setLoading] = useState(true);
   const[currentPage , setCurrentPage] = useState(1);
   const itemsPerPage = 6;
 
+  useEffect(()=>{
+    setLoading(true);
+    fetch("jobs.json").then(res => res.json()).then(data =>{//console.log(data)})
+  setJobs(data)
+  setLoading(false)
+    })
+  },[])
 
+  const[query,setQuery]=useState(""); 
+  const handleInputChange =(event)=>{
+    setQuery(event.target.value)
+  }
+  const filteredItems= jobs?.filter((job)=> job.jobTitle.toLowercase().indexof(query.toLowerCase()) !== -1);
+  //console.log(filteredItems);
+ const handleChange = (event) =>{
+  setSelectedCategory(event.target.value)
+ }
+
+
+ const handleClick = (event)=>{
+  setSelectedCategory(event.target.value)
+ }
+
+ 
   const calculatePerRange = () =>{
     const startIndex = (currentPage -1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
@@ -22,12 +47,33 @@ const Home = () => {
     }
   }
 
-  //Function for previous page
-  const previousPage = () =>{
+   //Function for previous page
+  const prevPage = () =>{
     if(currentPage > 1){
       setCurrentPage(currentPage-1);
     }
   }
+
+ const filteredData =(jobs,selected,query)=>{
+  let filteredJobs =jobs;
+  if(query){
+    filteredJobs=filteredItems;
+  }
+  if(selected){
+    filteredJobs=filteredJobs.filter(({jobLocation,maxprice,experienceLevel,salaryType,employmentType,postingDate})=>(
+      jobLocation.toLowerCase() === selected.toLowerCase() ||
+      parseInt(maxprice) <= parseInt(selected) || 
+      salaryType.toLowerCase() === selected.toLowerCase() ||
+      employmentType.toLowerCase() === selected.toLowerCase()
+   ));
+   console.log(filteredData);
+    }
+    const {startIndex , endIndex} = calculatePerRange();
+    filteredJobs = filteredJobs.slice(startIndex , endIndex);
+    return filteredJobs.map((data,i)=><Card key={i} data={data}/>)
+ }
+ const result = filteredData(jobs,selectedCategory,query);
+ 
   return (
     <div>
       <Banner />
@@ -42,12 +88,21 @@ const Home = () => {
       {/* job cards */}
       <div className='col-span-2 bg-white p-4 rounded-sm'>
         {
-          isLoading ? (<p className='font-medium'>Loading...</p>) : result.length > 0 ? (<Jobs result={results}/>) :<>
-          <h3 className='text-lg font-bold mb-2'>{results.length} Jobs</h3>
+          isLoading ? (<p className='font-medium'>Loading...</p>) : result.length > 0 ? (<Jobs result={result}/>) :<>
+          <h3 className='text-lg font-bold mb-2'>{result.length} Jobs</h3>
           <p>No Data Found !!</p>
           </>
         }
          
+         {
+          result.length > 0 ? (
+            <div className='flex justify-center mt-4 space-x-8'>
+              <button onClick={prevPage} disabled={currentPage === 1}>Previous</button>
+              <span className='mx-2'>Page {currentPage} of {Math.ceil(filteredItems.length / itemsPerPage)}</span>
+              <button onClick={nextPage} disabled={currentPage === Math.ceil(filteredItems.length > itemsPerPage)} className='hover:underline'>Next</button>
+            </div>
+          ) : ""
+         }
       </div>
 
       {/* // rightSide */}

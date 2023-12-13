ec601eb2c398f8a5604a25e24459a2500da1e219
Vaibhav203 <bhav.bhaliya@gmail.com>
2023-12-13 19:18:13 +0530

    rahul

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+
+import ReactDOM from 'react-dom/client';
+import router from './Router/Router';
 
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
